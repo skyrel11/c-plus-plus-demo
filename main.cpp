a821fe2751f98ed2dd5b3cb2031e13d5d9657814
najwa rks plai <najwaizza@student.plai.ac.id>
2026-04-22 11:32:37 +0700

    Peran si A

--- main.cpp
+++ main.cpp
@@ -9,3 +9,14 @@ void displayHeader() {
     cout << "Status: Active Monitoring..." << endl;
     cout << "--------------------------------" << endl;
 }
+
+bool isSecure(string pwd) {
+    bool hasNum = false;
+    bool isLongEnough = (pwd.length() >= 8);
+    
+    for(char c : pwd) {
+        if(isdigit(c)) hasNum = true;
+    }
+    
+    return hasNum && isLongEnough;
+}
